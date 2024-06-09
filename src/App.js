07a9f6c5fe855d8a3df286838fc0fc6ceb8cf450
Artem <xfan08@mail.ru>
2024-06-09 17:45:28 +0800

    added toggle button and header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import { useEffect } from "react";
 import './App.css'
+import Header from "./components/Header/Header";
+import { useTelegram } from "./components/hooks/useTelegram";
 
-const tg = window.Telegram.WebApp;
 
 function App() {
+  const {tg, onToggleButton} = useTelegram()
 
   useEffect(() => {
     tg.ready();
   }, [])
 
 
-  const onClose = () => {
-    tg.close();
-  }
-
   return (
     <div className="App">
-        <button onClick={onClose}>Close</button>
+        <Header/>
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
