a69cfbf26680f3ffc793b04f1516a192edb09d97
Artem <xfan08@mail.ru>
2024-06-10 18:18:37 +0800

    added send func

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,53 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import './Form.css'
+import React, { useEffect, useState } from "react";
+import "./Form.css";
 
 const tg = window.Telegram.WebApp;
 
-
 const Form = () => {
-    const [count, setCount] = useState({
-      pay: '',
-      get: ''
-    })
-
-    useEffect(() => {
-      tg.MainButton.setParams({
-        text: 'Trade'
-      })
-    }, [])
-
+  const [count, setCount] = useState({
+    pay: "",
+    get: "",
+  });
 
   useEffect(() => {
-    if(!count.pay || !count.get){
+    tg.onEvent("mainButtonClicked", () => {
+      tg.sendData({
+        pay: count.pay,
+        get: count.get,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Trade",
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!count.pay || !count.get) {
       tg.MainButton.hide();
-      console.log("hide")
-    }else{
-      tg.MainButton.show()
-      console.log("show")
+      console.log("hide");
+    } else {
+      tg.MainButton.show();
+      console.log("show");
     }
-  }, [count.pay, count.get])
+  }, [count.pay, count.get]);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-    const handleChange = (e) => {
-      const {name, value} = e.target;
+    setCount({
+      ...count,
+      [name]: value,
+    });
+  };
 
-      setCount({
-        ...count,
-        [name]: value
-      })
-    }
-
-    console.log(count)
-
-
+  console.log(count);
 
   return (
-    <div className='form'>
-            <input type="text" className='input' placeholder='Pay' name='pay' value={count.pay} onChange={(e) => handleChange(e)}/>
-            <input type="text" className='input' placeholder='Get' name='get' value={count.get} onChange={(e) => handleChange(e)}/>
-
+    <div className="form">
+      <input
+        type="text"
+        className="input"
+        placeholder="Pay"
+        name="pay"
+        value={count.pay}
+        onChange={(e) => handleChange(e)}
+      />
+      <input
+        type="text"
+        className="input"
+        placeholder="Get"
+        name="get"
+        value={count.get}
+        onChange={(e) => handleChange(e)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
