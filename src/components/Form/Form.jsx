8e14d60c01932b8d8be3575ecbff47db3b9d6f59
Artem <xfan08@mail.ru>
2024-06-10 18:04:47 +0800

    added new func

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Form.css'
 
+const tg = window.Telegram.WebApp;
+
+
 const Form = () => {
+    const [count, setCount] = useState({
+      pay: '',
+      get: ''
+    })
+
+
+  useEffect(() => {
+    if(!count.pay || !count.get){
+      tg.MainButton.hide();
+      console.log("hide")
+    }else{
+      tg.MainButton.show()
+      console.log("show")
+    }
+  }, [count.pay, count.get])
+
+
+    const handleChange = (e) => {
+      const {name, value} = e.target;
+
+      setCount({
+        ...count,
+        [name]: value
+      })
+    }
+
+    console.log(count)
+
+
+
   return (
     <div className='form'>
-            <input type="text" className='input' placeholder='Pay'/>
-            <input type="text" className='input' placeholder='Get'/>
-            <button className='button'>Send</button>
+            <input type="text" className='input' placeholder='Pay' name='pay' value={count.pay} onChange={(e) => handleChange(e)}/>
+            <input type="text" className='input' placeholder='Get' name='get' value={count.get} onChange={(e) => handleChange(e)}/>
+
     </div>
   )
 }
